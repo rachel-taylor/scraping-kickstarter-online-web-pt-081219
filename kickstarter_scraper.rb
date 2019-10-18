bcd0d64413e5f358a3bf3c0a2e13933677eee777
Rachel T <rachmaybe@gmail.com>
2019-10-18 22:23:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,10 @@ File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   
    projects = {}
-   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
  
   projects
